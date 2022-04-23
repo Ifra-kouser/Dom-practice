7ab89679cf6499cc9146054f72b1f0aedf1b5cd2
ifravlog <mohammediifra354@gmail.com>
2022-04-23 20:34:53 +0530

    my third commit

---
+++ index7.js
@@ -0,0 +1,14 @@
+let titlenew = document.title = "DomIntro";
+console.log(titlenew);
+
+let element = document.getElementById('unique');
+console.log(element);
+
+let ele = document.getElementsByClassName('multi');
+console.log(ele);
+
+let ele2 = document.querySelector('.multi');
+console.log(ele2);
+
+let ele3 = document.querySelectorAll('.multi');
+console.log(ele3);
